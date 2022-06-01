ec4ac699d6e48800b474b9c3d1c785e1b75e9eb6
asmasaeed <asma.saeedh@gmail.com>
2022-06-01 19:30:43 +0800

    Create footer.php
    footer php code

---
+++ footer.php
@@ -0,0 +1,24 @@
+         <footer class="container-fluid"><!-- div for the main image to stretch s to the edge of the page-->
+            <div class="container"> <!-- div to hold the content in the middle of the page-->
+                <div class="row"> <!-- a row that gives us access to the BS columns-->
+                  <div class="col-md-6 text-left">
+                    <p>Stay Updated</p>
+                    <a class= "socialmedia" href="#">Instagram
+                    <br>Twitter
+                    <br>Facebook</a>
+                  </div>
+                  <div class="col-md-6 text-right">
+                    <p>Contact Us</p>
+                    <p>Fremantle arts centre
+                    <br>08 9432 9555
+                    <br>artscentre@fremantle.wa.gov.au
+                    </p>
+                  </div><!-- row-->
+                </div><!-- container -->
+              </div><!-- container-fluid-->
+        </footer>
+        
+        <?php wp_footer(); ?>
+
+</body>
+</html>
