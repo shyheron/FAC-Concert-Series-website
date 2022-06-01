dd71afde6881e2f4ddcf908d9d4da5145ab7c869
asmasaeed <asma.saeedh@gmail.com>
2022-06-01 20:01:20 +0800

    Update index.php
    photo url correct update

--- index.php
+++ index.php
@@ -5,37 +5,37 @@
             <div class="container">
                 <h2 class="text-center">Artist Lineup</h2>
                 <div class="row text-center"><!--  a row that gives us access to the BS columns-->
-  
+                
                 <div class="col-md-3">
-                    <img class="img-fluid" src="http://206.189.45.97/~mesh1/wp-content/themes/FAC-Concert-Series-website/images/artists/Anesu.png" alt="Artist">
+                    <img class="img-fluid" src="http://206.189.45.97/~mesh21/sub3/wp-content/themes/FAC-Concert-Series-website/images/artists/Anesu.png" alt="Artist">
                     <h4 class="more-title">Anesu</h4>
                 </div>
                 <div class="col-md-3">
-                    <img class="img-fluid" src="http://206.189.45.97/~mesh1/wp-content/themes/FAC-Concert-Series-website/images/artists/Ken Paolo and the Space Cadets.png" alt="Artist">
+                    <img class="img-fluid" src="http://206.189.45.97/~mesh21/sub3/wp-content/themes/FAC-Concert-Series-website/images/artists/Ken Paolo and the Space Cadets.png" alt="Artist">
                     <h4 class="more-title">Ken Paolo and the Space Cadets</h4>
                 </div>
                 <div class="col-md-3">
-                    <img class="img-fluid" src="http://206.189.45.97/~mesh1/wp-content/themes/FAC-Concert-Series-website/images/artists/Grievous Bodily Calm.png" alt="Artist">
+                    <img class="img-fluid" src="http://206.189.45.97/~mesh21/sub3/wp-content/themes/FAC-Concert-Series-website/images/artists/Grievous Bodily Calm.png" alt="Artist">
                     <h4 class="more-title">Grievous Bodily Calm</h4>
                 </div>
                 <div class="col-md-3">
-                  <img class="img-fluid" src="http://206.189.45.97/~mesh1/wp-content/themes/FAC-Concert-Series-website/images/artists/Michael Dunstan.png" alt="Artist">
+                  <img class="img-fluid" src="http://206.189.45.97/~mesh21/sub3/wp-content/themes/FAC-Concert-Series-website/images/artists/Michael Dunstan.png" alt="Artist">
                   <h4 class="more-title">Michael Dunstan</h4>
               </div>
                   <div class="col-md-3">
-                    <img class="img-fluid" src="http://206.189.45.97/~mesh1/wp-content/themes/FAC-Concert-Series-website/images/artists/Emily Barker.png" alt="Artist">
+                    <img class="img-fluid" src="http://206.189.45.97/~mesh21/sub3/wp-content/themes/FAC-Concert-Series-website/images/artists/Emily Barker.png" alt="Artist">
                     <h4 class="more-title">Emily Barker</h4>
                   </div>
                   <div class="col-md-3">
-                    <img class="img-fluid" src="http://206.189.45.97/~mesh1/wp-content/themes/FAC-Concert-Series-website/images/artists/Project Bexx.png" alt="Artist">
+                    <img class="img-fluid" src="http://206.189.45.97/~mesh21/sub3/wp-content/themes/FAC-Concert-Series-website/images/artists/Project Bexx.png" alt="Artist">
                     <h4 class="more-title">Project Bexx</h4>
                   </div>
                   <div class="col-md-3">
-                    <img class="img-fluid" src="http://206.189.45.97/~mesh1/wp-content/themes/FAC-Concert-Series-website/images/artists/Ocean Alley.png" alt="Artist">
+                    <img class="img-fluid" src="http://206.189.45.97/~mesh21/sub3/wp-content/themes/FAC-Concert-Series-website/images/artists/Ocean Alley.png" alt="Artist">
                     <h4 class="more-title">Ocean Alley</h4>
                   </div>
                   <div class="col-md-3">
-                    <img class="img-fluid" src="http://206.189.45.97/~mesh1/wp-content/themes/FAC-Concert-Series-website/images/artists/Maatakitj.png" alt="Artist">
+                    <img class="img-fluid" src="http://206.189.45.97/~mesh21/sub3/wp-content/themes/FAC-Concert-Series-website/images/artists/Maatakitj.png" alt="Artist">
                     <h4 class="more-title">Maatakitj</h4>
                   </div>
                 </div>
@@ -55,19 +55,19 @@
                 <div class="row text-center"><!--  a row that gives us access to the BS columns-->
                     
                     <div class="col-md-4">
-                        <img class="img-fluid" src="http://206.189.45.97/~mesh1/wp-content/themes/FAC-Concert-Series-website/images/events/Adult art class.png" alt="-">
+                        <img class="img-fluid" src="http://206.189.45.97/~mesh21/sub3/wp-content/themes/FAC-Concert-Series-website/images/events/Adult art class.png" alt="-">
                         <h4 class="more-title">Adult art courses</h4>
                         <p>Adult art courses that cater for all abilities from absolute beginners to experienced creatives.</p>
                         <a class= "readmore" href="#">Continue reading ></a>
                     </div>
                     <div class="col-md-4">
-                        <img class="img-fluid" src="http://206.189.45.97/~mesh1/wp-content/themes/FAC-Concert-Series-website/images/events/Kids art class.png" alt="-">
+                        <img class="img-fluid" src="http://206.189.45.97/~mesh21/sub3/wp-content/themes/FAC-Concert-Series-website/images/events/Kids art class.png" alt="-">
                         <h4 class="more-title">Kids art courses</h4>
                         <p>Classes for kids taught by highly skilled and experienced artists to ensure that your child gets the hands-on support and guidance needed to build their confidence and technical knowledge.</p>
                         <a class= "readmore" href="#">Continue reading ></a>
                     </div>
                     <div class="col-md-4">
-                        <img class="img-fluid" src="http://206.189.45.97/~mesh1/wp-content/themes/FAC-Concert-Series-website/images/events/Food & crafts market.png" alt="-">
+                        <img class="img-fluid" src="http://206.189.45.97/~mesh21/sub3/wp-content/themes/FAC-Concert-Series-website/images/events/Food & crafts market.png" alt="-">
                         <h4 class="more-title">Food & crafts market</h4>
                         <p>Our concerts generally run alongside a series of food and crafts market for shopping. We also have our own little bar station just for you!</p>
                         <a class= "readmore" href="#">Continue reading ></a>
