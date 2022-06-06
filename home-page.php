326765f393cf366345c71c0057d91669d869e1f4
asmasaeed <asma.saeedh@gmail.com>
2022-06-06 15:09:43 +0800

    Update home-page.php

--- home-page.php
+++ home-page.php
@@ -9,7 +9,7 @@
             <!-- BG2 ARTISTS-->
         <section class="container-fluid artistbg">
             <div class="container">
-                <h2><?php the_field('titleforaboutme'); ?></h2><!--  custom field for the aboutme content-->
+                <h2><?php the_field('titleforartistlineup'); ?></h2><!--  custom field for the aboutme content-->
                 <div class="row text-center">
                 
                <!--  <div class="col-md-3">
