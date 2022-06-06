0ea9ba630085ca2907129c461a28144e06edcc12
asmasaeed <asma.saeedh@gmail.com>
2022-06-06 21:33:33 +0800

    Update ticket-page-template.php

--- ticket-page-template.php
+++ ticket-page-template.php
@@ -8,9 +8,7 @@
     <!-- BG1-->
     <section class= "container-fluid covidinfo">
         <div class="container text-center covidinfosub">
-            <h3>YOU ARE REQUIRED TO PROVIDE PROOF OF  YOUR ID AND COVID VACCINATION BEFORE ENTERING THE SITE</h3>
-
-            <h3>By continuing past this page and placing your order, you are agreeing to the Terms and Conditions of purchase and for this event</h3>
+        <h3 class="article-title"><?php the_field('covidinfo'); ?></h3>
         </div> 
     </section>
 
@@ -20,18 +18,14 @@
         <div class="container">
             <div class="row">
             <div class="col-md-6 text-center">
-                    <h3>Order Summary</h3>
-                    <img class="img-fluid" alt="Artist" src="images/artists/Maatakitj.png">
-                  <p>Maatakitjz
-                  <br>2 April 4PM - 6PM
-                  <br>A little priarie song</p>
-                    
-                  <p>$50</p>
+                <h3 class="article-title"><?php the_field('ordersummary'); ?></h3>
+                <img class="img-fluid" alt=“moreventsimage” src="<?php the_field('ordersummaryimage');?>">
+                <p class="article-p"><?php the_field('ordersummaryexcerpt'); ?> </p>
                 </div>
                 
                 </div>
                 <div class="col-md-6">
-                    <h3>Personal Details</h3>
+                    <h3 class="article-title"><?php the_field('personaldetails'); ?></h3>
                     <form action="/action_page.php">
                         <div class="form-group">
                           <label for="fname">Full Name:</label>
@@ -42,7 +36,7 @@
                           <input type="password" class="form-control" placeholder="Enter contact number" id="cnumb">
                         </div>
                       </form>
-                      <h3>Payment Details</h3>
+                      <h3 class="article-title"><?php the_field('paymentdetails'); ?></h3>
                     <form action="/action_page.php" class="was-validated">
                       <div class="form-group">
                         <input type="text" class="form-control" placeholder="Card" id="paymentdetails">
