a7f8586bab5d706def2a87a45a44c4af8b8f0065
asmasaeed <asma.saeedh@gmail.com>
2022-06-06 15:05:13 +0800

    Update home-page.php
    home page wp

--- home-page.php
+++ home-page.php
@@ -1,17 +1,18 @@
 <?php
     /*
-    Template Name: FAC Concert Series Home page
+    Template Name: Home page
     */
     ?>
-<?php get_header();?>   
+
+<?php get_header();?>   <!--  Tells WordPress to include header.php -->
 
             <!-- BG2 ARTISTS-->
         <section class="container-fluid artistbg">
             <div class="container">
-                <h2 class="text-center">Artist Lineup</h2>
-                <div class="row text-center"><!--  a row that gives us access to the BS columns-->
+                <h2><?php the_field('titleforaboutme'); ?></h2><!--  custom field for the aboutme content-->
+                <div class="row text-center">
                 
-                <div class="col-md-3">
+               <!--  <div class="col-md-3">
                     <img class="img-fluid" src="http://206.189.45.97/~mesh21/sub3/wp-content/themes/FAC-Concert-Series-website/images/artists/Anesu.png" alt="Artist">
                     <h4 class="more-title">Anesu</h4>
                 </div>
@@ -42,8 +43,8 @@
                   <div class="col-md-3">
                     <img class="img-fluid" src="http://206.189.45.97/~mesh21/sub3/wp-content/themes/FAC-Concert-Series-website/images/artists/Maatakitj.png" alt="Artist">
                     <h4 class="more-title">Maatakitj</h4>
-                  </div>
-                </div>
+                  </div> -->
+                </div> 
 
                 <div class="text-center">
                   <h3>Enjoy a carefully curated lineup of the best local and touring musicians</h3>
@@ -56,10 +57,10 @@
         <!-- BG3 EVENTS-->
         <section class="container-fluid morebg">
             <div class="container morebgsub">
-                <h2 class="text-center">More Events</h2>
-                <div class="row text-center"><!--  a row that gives us access to the BS columns-->
+                <!-- <h2 class="text-center">More Events</h2>
+                <div class="row text-center">
                     
-                    <div class="col-md-4">
+                     <div class="col-md-4">
                         <img class="img-fluid" src="http://206.189.45.97/~mesh21/sub3/wp-content/themes/FAC-Concert-Series-website/images/events/Adult art class.png" alt="-">
                         <h4 class="more-title">Adult art courses</h4>
                         <p>Adult art courses that cater for all abilities from absolute beginners to experienced creatives.</p>
@@ -77,7 +78,7 @@
                         <p>Our concerts generally run alongside a series of food and crafts market for shopping. We also have our own little bar station just for you!</p>
                         <a class= "readmore" href="#">Continue reading ></a>
                     </div>
-                    </div><!-- row-->
+                    </div>  -->
 
                     <div class="text-center">
                         <a href="#" class="btn btn-default btn-lg">SEE ALL EVENTS</a>
