93011e34a4f12304585d4dc44021c67170c250d1
asmasaeed <asma.saeedh@gmail.com>
2022-06-07 20:49:12 +0800

    Update about-page-template.php
    update of map section to wp

--- about-page-template.php
+++ about-page-template.php
@@ -52,17 +52,19 @@
 <!-- BG3-->  
 <section class="container-fluid map">
     <div class="container mapsub">
-    <h2 class="subheading text-center">How To Get Here</h2>
+    <h2 class="article-title"><?php the_field('titleformap'); ?></h2>
       
     <div class="row"> <!--  a row that gives us access to the BS columns-->
         <div class="col-md-6">
-            <p>Free parking is available outside Fremantle Arts Centre on Finnerty Street, Ord Street and Vale Street
+          <p class="article-p"><?php the_field('mapexcerpt'); ?> </p>
+            <!-- <p>Free parking is available outside Fremantle Arts Centre on Finnerty Street, Ord Street and Vale Street
             <br>Paid parking is available at Fremantle Leisure Centre (Opposite Fremantle Arts Centre on Ord Street)
             <br>Public transport - Fremantle train station is located 1.2km away from Fremantle Arts Centre. You can walk from the station to Fremantle Arts Centre in approximately 15 minutes!
-            </p>          
+            </p>  -->         
         </div>
         <div class="col-md-6">
-            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3381.8103122328293!2d115.7542612472965!3d-32.0473254394626!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2a32a163d6c8943f%3A0x9e3a40df81e5279e!2sFremantle%20Arts%20Centre!5e0!3m2!1sen!2sau!4v1653974060325!5m2!1sen!2sau" width="100%" height="100%" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+            <!-- <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3381.8103122328293!2d115.7542612472965!3d-32.0473254394626!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2a32a163d6c8943f%3A0x9e3a40df81e5279e!2sFremantle%20Arts%20Centre!5e0!3m2!1sen!2sau!4v1653974060325!5m2!1sen!2sau" width="100%" height="100%" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe> -->
+              <img class="img-fluid" alt=“moreventsimage” src="<?php the_field('mapimage');?>">
             </div>
       </div>
     </div>
