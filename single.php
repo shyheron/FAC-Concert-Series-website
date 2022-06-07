7e2ef7c41ae91b5c814fdcae32a06cf23286f359
asmasaeed <asma.saeedh@gmail.com>
2022-06-07 22:18:09 +0800

    Update single.php

--- single.php
+++ single.php
@@ -1,4 +1,4 @@
-<?php require_once('artist-post-header.php'); ?>
+<?php require_once('history-post-header.php'); ?>
 <div class="container-fluid maincontainer">
     <div class="container">
         <div class="row">
