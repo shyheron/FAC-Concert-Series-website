7b450eb71592ee147a5958b44d811060082bd5c4
asmasaeed <asma.saeedh@gmail.com>
2022-06-08 00:51:38 +0800

    Update about-page-template.php

--- about-page-template.php
+++ about-page-template.php
@@ -16,37 +16,32 @@
 
     <!-- BG2-->    
     <section class="container-fluid aboutintro">
-    <div class="container">
-      <div class="row">
-      <div class="col-md-6">
-      <h4 class="article-title"><?php the_field('titleforhistory'); ?></h4>
-        <img class="img-fluid" alt=“moreventsimage” src="<?php the_field('historyimage');?>">
-      </div>
-      <div class="col-md-6">
-        <p class="article-p"><?php the_field('historyexcerpt'); ?> </p>
-        <a href="<?php the_field('historyreadmore'); ?>" class="readmore">Continue Reading</a>
-      </div> 
+      <div class="container">
 
-      <div class="col-md-6">
-      <h4 class="article-title"><?php the_field('titleforvisualarts'); ?></h4>
-        <img class="img-fluid" alt=“moreventsimage” src="<?php the_field('visualartsimage');?>">
-      </div>
-      <div class="col-md-6">
-        <p class="article-p"><?php the_field('visualartsexcerpt'); ?> </p>
-        <a href="<?php the_field('visualartsreadmore'); ?>" class="readmore">Continue Reading</a>
-      </div>
+          <div class="row text-center"><!--  a row that gives us access to the BS columns-->
+              
+              <div class="col-md-4">
+                  <img class="img-fluid" alt=“moreventsimage” src="<?php the_field('historyimage');?>">
+                  <h4 class="article-title"><?php the_field('titleforhistory'); ?></h4>
+                  <p class="article-p"><?php the_field('historyexcerpt'); ?> </p>
+                  <a href="<?php the_field('specialeventsreadmore'); ?>" class="readmore">Continue Reading</a>
+              </div>
+              <div class="col-md-4">
+                  <img class="img-fluid" alt=“moreventsimage” src="<?php the_field('visualartsimage');?>">
+                  <h4 class="article-title"><?php the_field('titleforvisualarts'); ?></h4>
+                  <p class="article-p"><?php the_field('visualartsexcerpt'); ?> </p>
+                  <a href="<?php the_field('specialeventsreadmore'); ?>" class="readmore">Continue Reading</a>
+              </div>
+              <div class="col-md-4">
+                  <img class="img-fluid" alt=“moreventsimage” src="<?php the_field('specialeventsimage');?>">
+                  <h4 class="article-title"><?php the_field('titleforspecialevents'); ?></h4>
+                  <p class="article-p"><?php the_field('specialeventsexcerpt'); ?> </p>
+                  <a href="<?php the_field('specialeventsreadmore'); ?>" class="readmore">Continue Reading</a>
+              </div>
+              </div><!-- row-->
 
-      <div class="col-md-6">
-      <h4 class="article-title"><?php the_field('titleforspecialevents'); ?></h4>
-        <img class="img-fluid" alt=“moreventsimage” src="<?php the_field('specialeventsimage');?>">
-      </div>
-      <div class="col-md-6">
-        <p class="article-p"><?php the_field('specialeventsexcerpt'); ?> </p>
-        <a href="<?php the_field('specialeventsreadmore'); ?>" class="readmore">Continue Reading</a>
-      </div>
-    
-    </div>
-    <section>
+          </div><!-- container-->
+         </section><!-- container-fluid-->
 
 
 <!-- BG3-->  
