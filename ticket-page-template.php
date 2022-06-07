5402446f20e52c638339ff5973ff2efc8bdf11b4
asmasaeed <asma.saeedh@gmail.com>
2022-06-07 20:31:37 +0800

    Update ticket-page-template.php
    row and column opposite change

--- ticket-page-template.php
+++ ticket-page-template.php
@@ -14,39 +14,38 @@
 
 
     <!-- BG2 LEFT -->
-    <section class="container-fluid ticketsummarybg">
-        <div class="container">
-            <div class="row">
-            <div class="col-md-6 text-center">
-                <h3 class="article-title"><?php the_field('ordersummary'); ?></h3>
-                <img class="img-fluid" alt=“moreventsimage” src="<?php the_field('ordersummaryimage');?>">
-                <p class="article-p"><?php the_field('ordersummaryexcerpt'); ?> </p>
-                </div>
-                
-                </div>
-                <div class="col-md-6">
-                    <h3 class="article-title"><?php the_field('personaldetails'); ?></h3>
-                    <form action="/action_page.php">
-                        <div class="form-group">
-                          <label for="fname">Full Name:</label>
-                          <input type="email" class="form-control" placeholder="Enter full name" id="fname">
-                        </div>
-                        <div class="form-group">
-                          <label for="pwd">Contact number:</label>
-                          <input type="password" class="form-control" placeholder="Enter contact number" id="cnumb">
-                        </div>
-                      </form>
-                      <h3 class="article-title"><?php the_field('paymentdetails'); ?></h3>
-                    <form action="/action_page.php" class="was-validated">
-                      <div class="form-group">
-                        <input type="text" class="form-control" placeholder="Card" id="paymentdetails">
-                        <input type="text" class="form-control" placeholder="Paypal" id="paymentdetails">
-                        <input type="text" class="form-control" placeholder="Afterpay" id="paymentdetails">
-                      </div>
-                          <button type="submit" class="btn btn-primary">Confirm</button>
-                      </form>
+<section class="container-fluid ticketsummarybg">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-6 text-center">
+        <h3 class="article-title"><?php the_field('ordersummary'); ?></h3>
+        <img class="img-fluid" alt=“moreventsimage” src="<?php the_field('ordersummaryimage');?>">
+        <p class="article-p"><?php the_field('ordersummaryexcerpt'); ?> </p>
+        </div>
+      <div class="col-md-6">
+        <h3 class="article-title"><?php the_field('personaldetails'); ?></h3>
+        <form action="/action_page.php">
+            <div class="form-group">
+              <label for="fname">Full Name:</label>
+              <input type="email" class="form-control" placeholder="Enter full name" id="fname">
             </div>
+            <div class="form-group">
+              <label for="pwd">Contact number:</label>
+              <input type="password" class="form-control" placeholder="Enter contact number" id="cnumb">
+            </div>
+          </form>
+          <h3 class="article-title"><?php the_field('paymentdetails'); ?></h3>
+        <form action="/action_page.php" class="was-validated">
+          <div class="form-group">
+            <input type="text" class="form-control" placeholder="Card" id="paymentdetails">
+            <input type="text" class="form-control" placeholder="Paypal" id="paymentdetails">
+            <input type="text" class="form-control" placeholder="Afterpay" id="paymentdetails">
           </div>
+              <button type="submit" class="btn btn-primary">Confirm</button>
+          </form>
+        </div>
+    </div>
+  </div>
 </section>
 
 <?php get_footer(); ?>
