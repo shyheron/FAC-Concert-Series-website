15bfc315b9ce7df3cb165ea7609a03618d91683e
asmasaeed <asma.saeedh@gmail.com>
2022-06-08 10:45:58 +0800

    Update about-page-template.php

--- about-page-template.php
+++ about-page-template.php
@@ -18,27 +18,38 @@
     <section class="container-fluid aboutintro">
       <div class="container">
 
-          <div class="row text-center"><!--  a row that gives us access to the BS columns-->
-              
-              <div class="col-md-4">
-                  <img class="img-fluid" alt=“moreventsimage” src="<?php the_field('historyimage');?>">
-                  <h4 class="article-title"><?php the_field('titleforhistory'); ?></h4>
-                  <p class="article-p"><?php the_field('historyexcerpt'); ?> </p>
-                  <a href="<?php the_field('specialeventsreadmore'); ?>" class="readmore">Continue Reading</a>
-              </div>
-              <div class="col-md-4">
-                  <img class="img-fluid" alt=“moreventsimage” src="<?php the_field('visualartsimage');?>">
-                  <h4 class="article-title"><?php the_field('titleforvisualarts'); ?></h4>
-                  <p class="article-p"><?php the_field('visualartsexcerpt'); ?> </p>
-                  <a href="<?php the_field('specialeventsreadmore'); ?>" class="readmore">Continue Reading</a>
-              </div>
-              <div class="col-md-4">
-                  <img class="img-fluid" alt=“moreventsimage” src="<?php the_field('specialeventsimage');?>">
-                  <h4 class="article-title"><?php the_field('titleforspecialevents'); ?></h4>
-                  <p class="article-p"><?php the_field('specialeventsexcerpt'); ?> </p>
-                  <a href="<?php the_field('specialeventsreadmore'); ?>" class="readmore">Continue Reading</a>
-              </div>
-              </div><!-- row-->
+      <?php 
+$args = array(
+  'category_name' =>  'AboutPosts',
+ // 'posts_per_page'    =>   1
+);
+// the query
+$the_query = new WP_Query( $args ); ?>
+ 
+<?php if ( $the_query->have_posts() ) : ?>
+ 
+    <!-- pagination here -->
+ 
+    <!-- the loop -->
+    <?php while ( $the_query->have_posts() ) : $the_query->the_post(); ?>
+    <h2><?php the_title(); ?></h2> 
+    <div>
+<?php
+                if ( has_post_thumbnail() ) { // check if the post has a Post Thumbnail assigned to it.
+                    the_post_thumbnail('thumbnail');
+                } ?>
+<p class="about-text"><?php the_excerpt(); ?></p></div>
+    <a class="readmore" href="<?php the_permalink(); ?>"> CONTINUE READING</a>
+    <?php endwhile; ?>
+    <!-- end of the loop -->
+ 
+    <!-- pagination here -->
+ 
+    <?php wp_reset_postdata(); ?>
+ 
+<?php else : ?>
+    <p><?php _e( 'Sorry, no posts matched your criteria.' ); ?></p>
+<?php endif; ?>
 
           </div><!-- container-->
          </section><!-- container-fluid-->
