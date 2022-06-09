7277b164c3d33a2667f7cac99e4acc383e2dfffc
asmasaeed <asma.saeedh@gmail.com>
2022-06-10 01:12:42 +0800

    Update header.php

--- header.php
+++ header.php
@@ -41,7 +41,8 @@
         <div class="container"> <!-- keeps navbar inside the div -->
           <nav class="navbar navbar-expand-md navbar-dark">
             <!-- Brand -->
-            <img class="img-fluid" alt=“logo” src="<?php the_field('logo');?>">
+            <!-- <img class="img-fluid" alt=“logo” src="<?php the_field('logo');?>"> --->
+            <img class="img-fluid logo" src="http://206.189.45.97/~mesh21/sub3/wp-content/themes/starter_for_josh_site/images/logo.png" alt="Logo"> 
           
         <!-- Toggler/collapsibe Button -->
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-controls="bs-example-navbar-collapse-1" aria-expanded="false" aria-label="<?php esc_attr_e( 'Toggle navigation', 'your-theme-slug' ); ?>">
