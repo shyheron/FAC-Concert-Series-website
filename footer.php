569ee2f5fb7957cedc767f59465c81b1ffae3dff
asmasaeed <asma.saeedh@gmail.com>
2022-06-10 02:56:28 +0800

    Update footer.php

--- footer.php
+++ footer.php
@@ -2,22 +2,23 @@
             <div class="container"> <!-- div to hold the content in the middle of the page-->
                 <div class="row"> <!-- a row that gives us access to the BS columns-->
                   <div class="col-md-6 text-left">
-                    <p>Stay Updated</p>
-                    <?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('footer-left') ) : 
+                  <?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('footer-left') ) : 
 endif; ?>
-
+                    <!-- <p>Stay Updated</p>
                     <ul>
                     <li class= "socialmedia"> <a href="#">Instagram</a> </li>
                     <li class= "socialmedia"> <a href="#">Twitter</a> </li>
                     <li class= "socialmedia"> <a href="#">Facebook</a> </li>
-                     </ul>
+                    </ul> -->
                 </div>
                   <div class="col-md-6 text-right">
-                    <p>Contact Us</p>
+                  <?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('footer-right') ) : 
+endif; ?>
+                    <!-- <p>Contact Us</p>
                     <p>Fremantle arts centre
                     <br>08 9432 9555
                     <br>artscentre@fremantle.wa.gov.au
-                    </p>
+                    </p> -->
                   </div><!-- row-->
                 </div><!-- container -->
               </div><!-- container-fluid-->
