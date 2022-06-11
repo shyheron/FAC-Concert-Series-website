ef5dbb462a319de3df2035862f1f69ad0e4f01bb
asmasaeed <asma.saeedh@gmail.com>
2022-06-11 22:57:42 +0800

    Update history-post-header.php

--- history-post-header.php
+++ history-post-header.php
@@ -41,7 +41,7 @@
         <div class="container"> <!-- keeps navbar inside the div -->
           <nav class="navbar navbar-expand-md navbar-dark">
             <!-- Brand -->
-            <a class="navbar-brand" href="#">FAC music series</a> <!-- add image -->
+            <a href="http://206.189.45.97/~mesh21/sub3/"><img class="img-fluid logo" src=http://206.189.45.97/~mesh21/sub3/wp-content/themes/FAC-Concert-Series-website/images/logo.png alt="Logo"></a>
           
             <!-- Toggler/collapsibe Button -->
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
